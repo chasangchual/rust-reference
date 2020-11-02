de88f407e0a59ac7f90e4fb6034860f13ef90860
Sangchual Cha <scha@cwretirement.com>
2020-11-01 21:53:37 -0500

    RefCell borrow_mut() reference codes

--- src/concurrency/smartpointer/src/main.rs
+++ src/concurrency/smartpointer/src/main.rs
@@ -2,9 +2,11 @@ use smartpointer::simplebox::*;
 use smartpointer::rc::*;
 
 fn main() {
-    box_simple();
-    rc_simple();
+    //box_simple();
+    //rc_simple();
 
-    rc_cell_simple();
+    // rc_cell_simple();
+    rc_cell_clone();
+    //rc_cell_ref_map();
 }
 
--- src/concurrency/smartpointer/src/rc/mod.rs
+++ src/concurrency/smartpointer/src/rc/mod.rs
@@ -47,12 +47,9 @@ fn rc_simple_pointer<T>(ptr:&Rc<T>) {
     println!("rc pointer count: {:?}", Rc::as_ptr(ptr));
 }
 
-
-
-
 pub fn rc_cell_simple() {
     let int_pointer = Rc::new(RefCell::new(55));
-    
+
     let val2: Ref<_> = int_pointer.borrow();
     println!("rc_cell_simple {}", val2);
 
@@ -62,4 +59,41 @@ pub fn rc_cell_simple() {
     *val1 = *val1 + 100;
     println!("rc_cell_simple {}", val1);
 
+}
+
+pub fn rc_cell_clone() {
+    let int_pointer = Rc::new(RefCell::new(55));
+    
+    let int_pointer1 = int_pointer.clone();
+    let int_pointer2 = int_pointer.clone();
+    let int_pointer3 = int_pointer.clone();
+    
+    rc_simple_pointer(&int_pointer);
+
+    rc_simple_pointer(&int_pointer1);
+    rc_simple_pointer(&int_pointer1);
+    rc_simple_pointer(&int_pointer1);
+
+    let mut val1: RefMut<i32> = int_pointer1.borrow_mut();
+    let mut val2: RefMut<i32> = int_pointer2.borrow_mut();
+    let mut val3: RefMut<i32> = int_pointer3.borrow_mut();
+
+    println!("rc_cell_clone {}", val1);
+    println!("rc_cell_clone {}", val2);
+    println!("rc_cell_clone {}", val3);
+
+    *val1 = *val1 + 100;
+    *val2 = *val2 + 200;
+    *val3 = *val3 + 300;
+
+    println!("rc_cell_clone v2 {}", val1);
+    println!("rc_cell_clone v2 {}", val2);
+    println!("rc_cell_clone v2 {}", val3);
+}
+
+pub fn rc_cell_ref_map() {
+    let int_pointer = Rc::new(RefCell::new(55));
+
+    let mut val1: RefMut<i32> = int_pointer.borrow_mut();
+    let mut val2: RefMut<i32> = RefMut::map(val1, |t|  t);
 }
